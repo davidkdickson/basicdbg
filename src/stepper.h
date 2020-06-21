c18d46c310683799d660b4df828e15ccb9d646c5
David Dickson <davidkdickson@gmail.com>
2020-06-20 22:24:42 -0700

    clean up access to methods

--- src/stepper.h
+++ src/stepper.h
@@ -16,7 +16,10 @@ class Stepper {
   DebugInfo& m_debug_info;
 
   void handle_sigtrap(siginfo_t info);
+
   void single_step_instruction();
+  void step_over_breakpoint(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints);
+  void single_step_instruction_with_breakpoint_check(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints);
 
   public:
   Stepper(pid_t pid, uint64_t start_address, DebugInfo& debug_info)
@@ -24,13 +27,11 @@ class Stepper {
 
   void wait_for_signal();
 
-  void continue_execution(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints);
   void step_in(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints);
   void step_over();
   void step_out(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints);
 
-  void step_over_breakpoint(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints);
-  void single_step_instruction_with_breakpoint_check(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints);
+  void continue_execution(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints);
 
   void remove_breakpoint(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints, std::intptr_t addr);
   void set_breakpoint(std::unordered_map<std::intptr_t, Breakpoint>& breakpoints, std::intptr_t addr);
